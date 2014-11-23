3eae4e7a46aa2d7783452d6cd5b6c986cc9ca9a3
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-22 19:45:25 -0500

    Updated schema, thanks to RedditBotWrangler!

--- db_schema.sql
+++ db_schema.sql
@@ -2,11 +2,11 @@ CREATE DATABASE IF NOT EXISTS ShittyRemindMeBotDB;
 USE ShittyRemindMeBotDB;
 
 DROP TABLE IF EXISTS message_data;
-CREATE TABLE message_data   (
-                                id INT AUTO_INCREMENT NOT NULL,
-                                permalink VARCHAR(100),
-                                message LONGTEXT,
-                                new_date DATETIME,
-                                userID VARCHAR(20),
-                                PRIMARY KEY (id)
-                            );
+CREATE TABLE `message_data`   (
+                                `id` INT(11) AUTO_INCREMENT NOT NULL,
+                                `permalink` VARCHAR(400) NOT NULL DEFAULT '',
+                                `message` LONGTEXT/*VARCHAR(11000)*/ DEFAULT NULL,
+                                `new_date` DATETIME DEFAULT NULL,
+                                `userID` VARCHAR(50) DEFAULT NULL,
+                                PRIMARY KEY (`id`)
+                            )ENGINE=InnoDB AUTO_INCREMENT=45666 DEFAULT CHARSET=latin1;
